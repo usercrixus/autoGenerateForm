01dca3459c691b9a263f132f433fb627503bef89
Adrien CHAISNEAU <achaisneau@altyor.com>
2022-09-23 10:40:56 +0200

    stuck one fixed git log form work with state

--- src/Components/Pages/Main/Element/FormGenerator.tsx
+++ src/Components/Pages/Main/Element/FormGenerator.tsx
@@ -6,17 +6,24 @@ import FormJson from "../../../../dataStructure";
 import "./formGenerator.css";
 
 export default class FormGenerator
-  extends React.Component<{}, { Pagination: number, FormComponents: typeof FormJson, Data: any, listElement: any[][], name: string; }>
-  implements ProviderInterface {
-  bufferData: any[][] = [];
+  extends React.Component<
+    {},
+    {
+      Pagination: number;
+      dataStructure: typeof FormJson;
+    }
+  >
+  implements ProviderInterface
+{
   constructor(props: any) {
     super(props);
-    this.state = { Pagination: 0, FormComponents: FormJson, Data: {}, listElement: [[]], name: "alo" };
+    this.state = {
+      Pagination: 0,
+      dataStructure: JSON.parse(JSON.stringify(FormJson)),
+    };
   }
 
   componentDidMount() {
-    this.generateComponent();
-    this.setState({ FormComponents: this.state.FormComponents });
     BasicProvider.subscribe(this);
   }
   componentWillUnmount() {
@@ -26,17 +33,22 @@ export default class FormGenerator
     this.forceUpdate();
   }
 
-  generateComponent() {
+  generateComponent(): any[][] {
     let page = 0;
-    this.state.FormComponents.forEach((branch: any) => {
-      this.bufferData.push([]);
-      this.bufferData[page].push((<h1 key={branch.branchName}>{branch.sectionName}</h1>));
-      branch.formStructure.forEach((formStructure: any) => {
-        this.bufferData[page].push(this.dispatchInputType(formStructure));
-      });
-      page++;
+    let components: any[][] = [];
+    this.state.dataStructure.forEach((branch: any) => {
+      if (branch.isDisplayed) {
+        components.push([]);
+        components[page].push(
+          <h1 key={branch.branchName}>{branch.sectionName}</h1>
+        );
+        branch.formStructure.forEach((formStructure: any) => {
+          components[page].push(this.dispatchInputType(formStructure));
+        });
+        page++;
+      }
     });
-    this.setState({ listElement: this.bufferData });
+    return components;
   }
 
   dispatchInputType(formStructure: any): any {
@@ -56,20 +68,23 @@ export default class FormGenerator
   }
 
   generateTextField(formStructure: any): any {
-    console.log("okok");
-    this.state.Data[formStructure.name] = "";
     return (
       <div className="column formElement" key={formStructure.name}>
         <h2>{formStructure.question}</h2>
         <div>{formStructure.description}</div>
-        <input type="text" value={this.state.name} name={formStructure.name} onChange={this.eventTextField.bind(this)}></input>
+        <input
+          type="text"
+          value={formStructure.value}
+          name={formStructure.name}
+          onChange={(e) => this.eventTextField(e, formStructure)}
+        ></input>
       </div>
     );
   }
 
-  eventTextField(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ name: e.target.value });
-    console.log(e.target.value);
+  eventTextField(e: React.ChangeEvent<HTMLInputElement>, field: any) {
+    field.value = e.target.value;
+    this.setState({ dataStructure: this.state.dataStructure });
   }
 
   generateMultiChoice(formStructure: any): any {
@@ -77,14 +92,15 @@ export default class FormGenerator
       <div className="column formElement" key={formStructure.name}>
         <h2>{formStructure.question}</h2>
         <div>{formStructure.description}</div>
-        {formStructure.multiChoice.map((element: string) => {
+        {formStructure.multiChoice.map((element: string, index: number) => {
           return (
             <div key={element}>
               <input
                 type="checkbox"
                 name={formStructure.question}
                 value={formStructure.value}
-                onChange={(e) => formStructure.value = e.target.value}
+                checked={formStructure.value[index]}
+                onChange={(e) => this.eventMultiChoice(e, formStructure, index)}
               />
               <label htmlFor={element}>{element}</label>
             </div>
@@ -94,18 +110,31 @@ export default class FormGenerator
     );
   }
 
+  eventMultiChoice(
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: any,
+    index: number
+  ) {
+    field.value[index] = e.target.checked;
+    this.setState({ dataStructure: this.state.dataStructure });
+  }
+
   generateRadio(formStructure: any): any {
     return (
       <div className="column formElement" key={formStructure.name}>
         <h2>{formStructure.question}</h2>
         <div>{formStructure.description}</div>
-        {formStructure.radio.map((element: string) => {
+        {formStructure.radio.map((element: string, index: number) => {
           return (
             <div key={element}>
               <input
                 type="radio"
                 name={formStructure.question}
                 value={element}
+                checked={formStructure.value[index]}
+                onChange={(e) =>
+                  this.eventRadio(e, formStructure, element, index)
+                }
               />
               <label htmlFor={element}>{element}</label>
             </div>
@@ -113,6 +142,39 @@ export default class FormGenerator
         })}
       </div>
     );
+  }
+
+  eventRadio(
+    e: React.ChangeEvent<HTMLInputElement>,
+    field: any,
+    value: string,
+    index: number
+  ) {
+    console.log(field.value);
+
+    for (let i = 0; i < field.value.length; i++) {
+      if (i !== index) {
+        field.value[i] = false;
+      } else {
+        field.value[i] = true;
+      }
+    }
+
+    if (field.isBranch) {
+      field.radio.forEach((radioValue: any) => {
+        this.state.dataStructure.forEach((branch: any) => {
+          if (branch.branchName === radioValue) {
+            if (radioValue === value) {
+              branch.isDisplayed = true;
+            } else {
+              branch.isDisplayed = false;
+            }
+          }
+        });
+      });
+    }
+
+    this.setState({ dataStructure: this.state.dataStructure });
   }
 
   getSectionByName(branchName: string) {
@@ -132,19 +194,20 @@ export default class FormGenerator
 
   previousPage() {
     this.setState({ Pagination: this.state.Pagination - 1 });
-
   }
 
-
   render() {
+    let components = this.generateComponent();
     return (
       <div className="column">
-        <>
-          {this.state.listElement[this.state.Pagination]}
-        </>
-        {this.generateTextField(this.state.FormComponents[0].formStructure[0])}
         <div>
-          <button disabled={this.state.Pagination === 0} onClick={this.previousPage.bind(this)}>Previous</button>
+          {components[this.state.Pagination]}
+          <button
+            disabled={this.state.Pagination === 0}
+            onClick={this.previousPage.bind(this)}
+          >
+            Previous
+          </button>
           <button onClick={this.nextPage.bind(this)}>Next</button>
         </div>
       </div>
--- src/dataStructure.tsx
+++ src/dataStructure.tsx
@@ -1,106 +1,95 @@
 var dataStructure: any = [
-    {
-        "branchName": "start",
-        "isDisplayed": true,
-        "sectionName": "Part one",
-        "formStructure": [
-            {
-                "name": "firstName",
-                "question": "First Name",
-                "description": "What's your first name",
-                "inputType": "textField",
-                "isBranch": false,
-                "component": [],
-                "value": "aa"
-            },
-            {
-                "name": "lastName",
-                "question": "Last Name",
-                "description": "What's your last name",
-                "inputType": "textField",
-                "isBranch": false,
-                "component": [],
-                "value": ""
-            },
-            {
-                "name": "nationality",
-                "question": "nationality",
-                "description": "What's your nationality",
-                "inputType": "radio",
-                "radio": ["french", "english"],
-                "isBranch": true,
-                "component": [],
-                "value": ""
-            }
-        ],
-        "component": [],
-        "next": "end"
-    },
-    {
-        "branchName": "french",
-        "isDisplayed": false,
-        "sectionName": "French",
-        "formStructure": [
-            {
-                "name": "favoriteAuthor",
-                "question": "Auteur préféré",
-                "description": "Indiquez quel est votre auteur préféré",
-                "inputType": "multiChoice",
-                "multiChoice": ["Hugo", "Voltaire"],
-                "isBranch": false,
-                "component": [],
-                "value": []
-            }
-        ],
-        "component": [],
-        "next": null
-    },
-    {
-        "branchName": "english",
-        "isDisplayed": false,
-        "sectionName": "English",
-        "formStructure": [
-            {
-                "name": "favoriteAuthor",
-                "question": "Favorite autor",
-                "description": "Enter your favorite autor",
-                "inputType": "multiChoice",
-                "multiChoice": ["Shakespeare", "Orwell"],
-                "isBranch": false,
-                "component": [],
-                "value": []
-            }
-        ],
-        "component": [],
-        "next": null
-    },
-    {
-        "branchName": "end",
-        "isDisplayed": true,
-        "sectionName": "Last questions",
-        "formStructure": [
-            {
-                "name": "comment",
-                "question": "Comment",
-                "description": "Have you some comment to do",
-                "inputType": "textField",
-                "isBranch": false,
-                "component": [],
-                "value": ""
-            },
-            {
-                "name": "validation",
-                "question": "Validation",
-                "description": "Copie this number and valid : 59422479",
-                "inputType": "textField",
-                "isBranch": false,
-                "component": [],
-                "value": ""
-            }
-        ],
-        "component": [],
-        "next": null
-    }
+  {
+    branchName: "start",
+    isDisplayed: true,
+    sectionName: "Part one",
+    formStructure: [
+      {
+        name: "firstName",
+        question: "First Name",
+        description: "What's your first name",
+        inputType: "textField",
+        isBranch: false,
+        value: "",
+      },
+      {
+        name: "lastName",
+        question: "Last Name",
+        description: "What's your last name",
+        inputType: "textField",
+        isBranch: false,
+        value: "",
+      },
+      {
+        name: "nationality",
+        question: "nationality",
+        description: "What's your nationality",
+        inputType: "radio",
+        radio: ["french", "english"],
+        isBranch: true,
+        value: [false, true],
+      },
+    ],
+    next: "end",
+  },
+  {
+    branchName: "french",
+    isDisplayed: false,
+    sectionName: "French",
+    formStructure: [
+      {
+        name: "favoriteAuthor",
+        question: "Auteur préféré",
+        description: "Indiquez quel est votre auteur préféré",
+        inputType: "multiChoice",
+        multiChoice: ["Hugo", "Voltaire"],
+        isBranch: false,
+        value: [true, true],
+      },
+    ],
+    next: null,
+  },
+  {
+    branchName: "english",
+    isDisplayed: false,
+    sectionName: "English",
+    formStructure: [
+      {
+        name: "favoriteAuthor",
+        question: "Favorite autor",
+        description: "Enter your favorite autor",
+        inputType: "multiChoice",
+        multiChoice: ["Shakespeare", "Orwell"],
+        isBranch: false,
+        value: [false, true],
+      },
+    ],
+    next: null,
+  },
+  {
+    branchName: "end",
+    isDisplayed: true,
+    sectionName: "Last questions",
+    formStructure: [
+      {
+        name: "comment",
+        question: "Comment",
+        description: "Have you some comment to do",
+        inputType: "textField",
+        isBranch: false,
+        value: "",
+      },
+      {
+        name: "validation",
+        question: "Validation",
+        description: "Copie this number and valid : 59422479",
+        inputType: "textField",
+        isBranch: false,
+        value: "",
+      },
+    ],
+    next: null,
+  },
 ];
 
 export default dataStructure;
